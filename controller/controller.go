0eae3ede245f3b59921909bdd965871f3fa9669f
Ankit Bharti <bhartiankit692@gmail.com>
2024-07-13 12:46:46 +0530

    Update controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -20,7 +20,7 @@ const colName string = "Watchlist"
 
 var collection *mongo.Collection
 
-//initalising Collection
+// initalising Collection
 func init() {
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
@@ -38,53 +38,51 @@ func init() {
 	collection = client.Database(dbName).Collection(colName)
 }
 
-
 //Helper Functions
 
-func insertOneMovie(movie model.Netflix){
+func insertOneMovie(movie model.Netflix) {
 	inserted, err := collection.InsertOne(context.Background(), movie)
 
-	if err!= nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-	
-	fmt.Println("Inserted Movie with Id",inserted.InsertedID)
+
+	fmt.Println("Inserted Movie with Id", inserted.InsertedID)
 }
 
-func updateOneMovie(movieId string){
-	id,_:=primitive.ObjectIDFromHex(movieId)
-	filter:=bson.M{"_id":id}
-	update:=bson.M{"$set":bson.M{"watched":true}}
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
 
-	result, _:=collection.UpdateOne(context.Background(),filter,update)
+	result, _ := collection.UpdateOne(context.Background(), filter, update)
 
 	fmt.Println("updated watched", result.ModifiedCount)
 }
 
-func deleteOneMovie(movieId string){
-	id,_:=primitive.ObjectIDFromHex(movieId)
-	filter:=bson.M{"_id":id}
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
 
-	result, _:=collection.DeleteOne(context.Background(),filter)
+	result, _ := collection.DeleteOne(context.Background(), filter)
 
-	fmt.Println("deleted",result)
+	fmt.Println("deleted", result)
 }
 
-func deleteAll(){
-	result ,_:=collection.DeleteMany(context.Background(),bson.D{{}})
+func deleteAll() {
+	result, _ := collection.DeleteMany(context.Background(), bson.D{{}})
 	fmt.Println("deleted All", result.DeletedCount)
 }
 
-
-func getAllMovies() []primitive.M{
-	curr,_:=collection.Find(context.Background(),bson.D{{}})
+func getAllMovies() []primitive.M {
+	curr, _ := collection.Find(context.Background(), bson.D{{}})
 
 	var movies []primitive.M
 
-	for curr.Next(context.Background()){
+	for curr.Next(context.Background()) {
 		var movie bson.M
-		err:= curr.Decode(&movie)
-		if err!=nil{
+		err := curr.Decode(&movie)
+		if err != nil {
 			log.Fatal(err)
 		}
 		movies = append(movies, movie)
@@ -94,18 +92,16 @@ func getAllMovies() []primitive.M{
 	return movies
 }
 
-
-
 //Actual Controllers
 
-func GetAllMovies(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","application/json")
+func GetAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	allMovies := getAllMovies()
 	json.NewEncoder(w).Encode(allMovies)
 }
 
-func CreateMovies(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","application/json")
+func CreateMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	w.Header().Set("Allow-Control-Allow-Methods", "POST")
 
 	var movie model.Netflix
@@ -114,18 +110,17 @@ func CreateMovies(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movie)
 }
 
-func MarkWatched(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","application/json")
+func MarkWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
 
 	parms := mux.Vars(r)
-	updateOneMovie(parms["id"])
+	updateOneMovie(parms["id"])  //url {id}
 	json.NewEncoder(w).Encode(parms["id"])
 }
 
-
-func DeleteONeMovie(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","application/json")
+func DeleteONeMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	w.Header().Set("Allow-Control-Allow-Methods", "POST")
 
 	parms := mux.Vars(r)
@@ -133,8 +128,8 @@ func DeleteONeMovie(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(parms["id"])
 }
 
-func DeleteAll(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("content-Type","application/json")
+func DeleteAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	w.Header().Set("Allow-Control-Allow-Methods", "POST")
 
 	deleteAll()
